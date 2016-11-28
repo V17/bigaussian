bugfix - derivatives now computed by numpy.gradient added clipping and normalization in *filter* functions added multiscale normalization in hessian complete cleanup of *filter* functions

main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 __author__ = 'Vojtech Vozab'
 import bigaussian
 import argparse
@@ -22,8 +23,12 @@ if __name__ == '__main__':
                                                                             'that take more than a couple seconds but needs'
                                                                             ' x times more memory, where x is the number'
                                                                             'of multiscale steps.')
-    args = parser.parse_args()
     multiprocessing.freeze_support()
+    bigaussian.filter2d("./input/hgauss15.png", "hgauss-np.jpg", sigma_foreground=2, sigma_background=0.8, step_size=1, number_steps=3)
+    bigaussian.parallel_filter2d("./input/hgauss15.png", "hgauss-p.jpg", sigma_foreground=2, sigma_background=0.8, step_size=1, number_steps=3)
+    bigaussian.filter3d("./input/hgauss15.mha", "hgauss15-np.mha", sigma_foreground=1, sigma_background=0.4, step_size=0.6, number_steps=4)
+    bigaussian.parallel_filter3d("./input/hgauss15.mha", "hgauss15-p.mha", sigma_foreground=1, sigma_background=0.4, step_size=0.6, number_steps=4)
+    args = parser.parse_args()
     if args.dimensions is None:
         extension = os.path.splitext(args.input)[1].strip().lower()
         if extension == '.mha':